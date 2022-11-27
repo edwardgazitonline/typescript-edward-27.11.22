6b693e9e9fdac808fe1f272b79e5ce81068b4d19
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2022-11-27 14:30:09 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -6,9 +6,11 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 function Person(age){
   this.age = age
-    this.growOld = () => this.age++;
+
+  this.growOld = () => this.age++;
 
 };
 var person = new Person(1);
 console.log(person.age);
+
 
